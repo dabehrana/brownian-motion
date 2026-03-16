9c88c50554a87edc8f44b415fb2aba66c7736ffb
VM dabehrana <daaneshbehrana@gmail.com>
2026-03-16 14:31:32 +0000

    allow multiple plots on same figure

--- 1D_brownian-motion.py
+++ 1D_brownian-motion.py
@@ -6,9 +6,10 @@ from helper_functions import get_int_from_user
 
 
 def main():
-    brownian_motion = simulate_1D_brownian_motion()
-    print(brownian_motion)
-    plot_series(brownian_motion)
+    brownian_motions = [0] * number_of_sample_paths
+    for i in range(number_of_sample_paths):
+        brownian_motions[i] = simulate_1D_brownian_motion()
+    plot_brownian_motions(brownian_motions)
 
 
 def simulate_1D_brownian_motion():
@@ -18,13 +19,15 @@ def simulate_1D_brownian_motion():
     return pd.Series(process, index=[time for time in timeline])
 
 
-def plot_series(series):
+def plot_brownian_motions(brownian_motions):
+    cmap = plt.cm.get_cmap("tab20", number_of_sample_paths)
     plt.figure(figsize=(20, 10))
-    series.plot()
+    for i in range(number_of_sample_paths):
+        brownian_motions[i].plot(color=cmap(i))
     plt.title("1D Brownian Motion")
     plt.xlabel("t")
     plt.ylabel("X(t)")
-    plt.grid(True, linestyle="--", alpha=0.6)
+    plt.grid(True, linewidth=0.2, linestyle="--", alpha=0.6)
     plt.legend()
     plt.tight_layout()
     plt.show()
@@ -32,11 +35,15 @@ def plot_series(series):
 
 if __name__ == "__main__":
     increment = get_float_from_user("How large should each increment to be? ")
+
     while True:
         max_time = get_int_from_user(
             "At what time should the simulation end? ")
         if (max_time > increment):
             break
+
     number_of_increments = (np.floor(max_time/increment)).astype(
         int) if max_time % increment == 0 else (np.floor(max_time/increment)).astype(int) + 1
+    
+    number_of_sample_paths = get_int_from_user("How many sample paths do you want? ")
     main()
