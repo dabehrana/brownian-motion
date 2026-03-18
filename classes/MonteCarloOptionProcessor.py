ae5f12b63d70d9de7aea832cfa10f87033aaa8a8
VM dabehrana <daaneshbehrana@gmail.com>
2026-03-18 11:45:42 +0000

    mc option pricing expectation calc: currently None

--- classes/MonteCarloOptionProcessor.py
+++ classes/MonteCarloOptionProcessor.py
@@ -1,9 +1,16 @@
-from GeometricBrownianMotionProcessor import GeometricBrownianMotionProcessor
+import numpy as np
+import pandas as pd
+from .GeometricBrownianMotionProcessor import GeometricBrownianMotionProcessor
 
 class MonteCarloOptionProcessor:
     
-    def __init__(self, geom_bm_processor : GeometricBrownianMotionProcessor, K):
+    def __init__(self, geom_bm_processor : GeometricBrownianMotionProcessor, strike_price):
         self.geom_bm_processor = geom_bm_processor
-        self.K = K
+        self.strike_price = strike_price
 
-        
+    
+    def calculate_payoff(self, gbms : pd.Series):
+        return np.maximum(gbms.tail(1).item() - self.strike_price, 0)
+    
+    def calculate_expectation(self, arr):
+        np.mean(arr)
--- monte_carlo_option_pricing.py
+++ monte_carlo_option_pricing.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from helper_functions import get_float_from_user
 from helper_functions import get_int_from_user
 from classes.OneDimBrownianMotionProcessor import OneDimBrownianMotionProcessor
@@ -5,7 +7,16 @@ from classes.GeometricBrownianMotionProcessor import GeometricBrownianMotionProc
 from classes.MonteCarloOptionProcessor import MonteCarloOptionProcessor
 
 def main():
-    pass
+    brownian_motions = [0] * number_of_sample_paths
+    gbms = [0] * number_of_sample_paths
+    call_payoffs = [0] * number_of_sample_paths
+    for i in range(number_of_sample_paths):
+        brownian_motions[i] = geom_bm_processor.simulate_1D_brownian_motion()
+        gbms[i] = geom_bm_processor.simulate_geometric_brownian_motion(brownian_motions[i])
+        call_payoffs[i] = monte_carlo_option_processor.calculate_payoff(gbms[i])
+    expectation = monte_carlo_option_processor.calculate_expectation(call_payoffs)
+    print(expectation)
+
 
 
 if __name__ == "__main__":
@@ -22,9 +33,10 @@ if __name__ == "__main__":
     
     mu = get_float_from_user("Please enter a value for drift: ")
     sigma = get_float_from_user("Please enter a value for volatility: ")
-    s_0 = get_int_from_user("Please enter the initial value: ")
+    s_0 = get_float_from_user("Please enter the initial value: ")
+    strike_price = get_float_from_user("Please enter a strike price: ")
 
     one_dim_bm_processor = OneDimBrownianMotionProcessor(increment, max_time, number_of_sample_paths)
     geom_bm_processor = GeometricBrownianMotionProcessor(one_dim_bm_processor, mu, sigma, s_0)
-    monte_carlo_option_processor = MonteCarloOptionProcessor(geom_bm_processor, K)
+    monte_carlo_option_processor = MonteCarloOptionProcessor(geom_bm_processor, strike_price)
     main()
