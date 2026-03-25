77dd3c467e228d03306aa1b382184360be6b17ee
VM dabehrana <daaneshbehrana@gmail.com>
2026-03-25 12:21:30 +0000

    introducing parallelisation

--- geometric_brownian_motion.py
+++ geometric_brownian_motion.py
@@ -1,21 +1,30 @@
 from classes.SimulationInitialiser import SimulationInitialiser
 from classes.OneDimBrownianMotionProcessor import OneDimBrownianMotionProcessor
 from classes.GeometricBrownianMotionProcessor import GeometricBrownianMotionProcessor
+from functools import partial
+import concurrent.futures
 
 
 def main():
     brownian_motions = [0] * number_of_sample_paths
     gbms = [0] * number_of_sample_paths
-    for i in range(number_of_sample_paths):
-        brownian_motions[i] = geom_bm_processor.simulate_1D_brownian_motion()
-        gbms[i] = geom_bm_processor.simulate_geometric_brownian_motion(
-            brownian_motions[i])
+
+    with concurrent.futures.ProcessPoolExecutor() as excecutor:
+        f = partial(run_simulations, geom_bm_processor=geom_bm_processor)
+        gbms = list(excecutor.map(f, brownian_motions, gbms, chunksize=100))
+
     print("Plotting simulations...")
     geom_bm_processor.plot_geometric_brownian_motions(gbms)
 
 
-if __name__ == "__main__":
+def run_simulations(brownian_motions, gbms, geom_bm_processor: GeometricBrownianMotionProcessor):
+    brownian_motions = geom_bm_processor.simulate_1D_brownian_motion()
+    gbms = geom_bm_processor.simulate_geometric_brownian_motion(
+        brownian_motions)
+    return gbms
 
+
+if __name__ == "__main__":
     simulation_initialiser = SimulationInitialiser()
     increment, max_time, number_of_sample_paths = simulation_initialiser.get_1D_bm_parameters()
     mu, sigma, s_0 = simulation_initialiser.get_geom_bm_parameters()
--- one_dim_brownian_motion.py
+++ one_dim_brownian_motion.py
@@ -1,17 +1,27 @@
 from classes.SimulationInitialiser import SimulationInitialiser
 from classes.OneDimBrownianMotionProcessor import OneDimBrownianMotionProcessor
+from functools import partial
+import concurrent.futures
+import time
 
 
 def main():
     brownian_motions = [0] * number_of_sample_paths
-    for i in range(number_of_sample_paths):
-        brownian_motions[i] = bm_processor.simulate_1D_brownian_motion()
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        f = partial(run_simulations, bm_processor=bm_processor)
+        brownian_motions = list(executor.map(
+            f, brownian_motions, chunksize=100))
     print("Plotting simulations...")
     bm_processor.plot_brownian_motions(brownian_motions)
 
 
-if __name__ == "__main__":
+def run_simulations(brownian_motions, bm_processor: OneDimBrownianMotionProcessor):
+    brownian_motions = bm_processor.simulate_1D_brownian_motion()
+    return brownian_motions
 
+
+if __name__ == "__main__":
     simulation_initialiser = SimulationInitialiser()
     increment, max_time, number_of_sample_paths = simulation_initialiser.get_1D_bm_parameters()
 
