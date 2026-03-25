0a3aa9a0b626f1449642a6f53173e0800d66c68c
VM dabehrana <daaneshbehrana@gmail.com>
2026-03-25 12:50:03 +0000

    parallelising mc

--- monte_carlo_option_pricing.py
+++ monte_carlo_option_pricing.py
@@ -2,6 +2,8 @@ from classes.SimulationInitialiser import SimulationInitialiser
 from classes.OneDimBrownianMotionProcessor import OneDimBrownianMotionProcessor
 from classes.GeometricBrownianMotionProcessor import GeometricBrownianMotionProcessor
 from classes.MonteCarloOptionProcessor import MonteCarloOptionProcessor
+from functools import partial
+import concurrent.futures
 
 
 def main():
@@ -9,12 +11,16 @@ def main():
     brownian_motions = [0] * number_of_sample_paths
     gbms = [0] * number_of_sample_paths
     call_payoffs = [0] * number_of_sample_paths
-    for i in range(number_of_sample_paths):
-        brownian_motions[i] = geom_bm_processor.simulate_1D_brownian_motion()
-        gbms[i] = geom_bm_processor.simulate_geometric_brownian_motion(
-            brownian_motions[i])
-        call_payoffs[i] = monte_carlo_option_processor.calculate_payoff(
-            gbms[i])
+
+    with concurrent.futures.ProcessPoolExecutor() as excecutor:
+        f1 = partial(run_simulations, geom_bm_processor=geom_bm_processor)
+        f2 = partial(calculate_payoffs,
+                     monte_carlo_option_processor=monte_carlo_option_processor)
+        gbms = list(excecutor.map(
+            f1, brownian_motions, gbms, chunksize=100))
+        call_payoffs = list(excecutor.map(
+            f2, gbms, call_payoffs, chunksize=100))
+
     expectation = monte_carlo_option_processor.calculate_expectation(
         call_payoffs)
     fair_price = monte_carlo_option_processor.calculate_fair_price(
@@ -23,6 +29,22 @@ def main():
         gbms[0])
     pct_error = monte_carlo_option_processor.calculate_error(
         black_scholes_estimate, fair_price)
+    print_results(fair_price, black_scholes_estimate, pct_error)
+
+
+def run_simulations(brownian_motions, gbms, geom_bm_processor: GeometricBrownianMotionProcessor):
+    brownian_motions = geom_bm_processor.simulate_1D_brownian_motion()
+    gbms = geom_bm_processor.simulate_geometric_brownian_motion(
+        brownian_motions)
+    return gbms
+
+
+def calculate_payoffs(gbms, call_payoffs, monte_carlo_option_processor: MonteCarloOptionProcessor):
+    call_payoffs = monte_carlo_option_processor.calculate_payoff(gbms)
+    return call_payoffs
+
+
+def print_results(fair_price, black_scholes_estimate, pct_error):
     print(f"Fair price estimate: {fair_price}")
     print(f"Black-Scholes estimate: {black_scholes_estimate}")
     print(f"Percentage error: {pct_error}%")
